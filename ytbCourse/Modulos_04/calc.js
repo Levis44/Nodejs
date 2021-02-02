cb35485e75c61ee7e7c9b13d3cd286c30e6fd5df
Levi <levi.ciarrocchi@gmail.com>
2021-02-01 22:36:06 -0300

    Módulos

---
+++ ytbCourse/Modulos_04/calc.js
@@ -0,0 +1,9 @@
+soma = (x,y) => {
+    return x + y;
+}
+
+sub = (x,y) => {
+    return x - y;
+}
+
+module.exports = {soma, sub};
---
+++ ytbCourse/Modulos_04/index.js
@@ -0,0 +1,4 @@
+const calc = require('./calc');
+
+console.log(calc.soma(3,2));
+console.log(calc.sub(3,2));
