6016d630aaaaf3831df0462ad32a293210d75611
Levi <levi.ciarrocchi@gmail.com>
2021-03-08 12:07:53 -0300

    api

---
+++ api/server.js
@@ -0,0 +1,23 @@
+const exŕess = require('express');
+const app = express();
+
+app.get("/clients", (req, res) => {
+    
+})
+
+app.post("/clients", (req, res) => {
+    
+})
+
+app.put("/clients", (req, res) => {
+    
+})
+
+app.delete("/clients", (req, res) => {
+    
+})
+
+
+app.listen(3000, () => {
+    console.log("Server running");
+})
