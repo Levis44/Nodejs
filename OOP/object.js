2796540bd6fea4f3b35826de9aa9cb02e8f434f1
Levi <levi.ciarrocchi@gmail.com>
2021-03-30 15:55:16 -0300

    Constructors, inheritance

--- OOP/object.js
+++ OOP/object.js
@@ -1,12 +1,42 @@
-let userOne = {
-  email: "ryu@gmail",
-  name: "ryu",
+class User {
+  constructor(name, email) {
+    this.email = email;
+    this.name = name;
+    this.score = 0;
+  }
 
   login() {
-    console.log(this.name);
-  },
+    console.log(this.name + " hello");
+    return this;
+  }
 
   logout() {
-    console.log(this.email);
-  },
-};
+    console.log(this.email + " bye");
+    return this;
+  }
+
+  updateScore() {
+    this.score++;
+    console.log(this.score);
+    // Method Chaining
+    return this;
+  }
+}
+
+class Admin extends User {
+  deleteUser(user) {
+    users = users.filter((u) => u.email != user.email);
+  }
+}
+
+let userOne = new User("Levi", "levi@gmail");
+let userTwo = new User("Larissa", "larissa@gmail");
+let admin = new Admin("Roberto", "roberto@gmail");
+
+let users = [userOne, userTwo, admin];
+
+admin.deleteUser(userOne);
+console.log(users);
+
+// Method Chaining
+// userOne.login().updateScore().logout();
