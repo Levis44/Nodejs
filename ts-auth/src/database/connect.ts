64acfead3651c3c9c4a11d06547b25066567ca79
Levi <levi.ciarrocchi@gmail.com>
2021-06-23 16:32:15 -0300

    Auth and JWT ready

--- ts-auth/src/database/connect.ts
+++ ts-auth/src/database/connect.ts
@@ -1,3 +1,3 @@
 import { createConnection } from "typeorm";
 
-createConnection().then(() => console.log("succes!"));
+createConnection().then(() => console.log("📥 Database connected!"));
--- ts-auth/src/index.ts
+++ ts-auth/src/index.ts
@@ -2,11 +2,11 @@ import "reflect-metadata";
 import express from "express";
 
 import "./database/connect";
-import routes from "./routes";
+import router from "./routes";
 
 const app = express();
 
-app.use(routes);
 app.use(express.json());
+app.use(router);
 
-app.listen(3000, () => console.log("Rodando!"));
+app.listen(3000, () => console.log("🔥 Application running!"));
